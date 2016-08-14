5193500a754c94e45336ea653c0567fc3619ece6
rbrahul95 <rbrahul95@gmail.com>
2016-08-15 04:21:29 +0530

    Update and rename  Session_2_Assignment_2.java to Session_2_Assignment_2.java

--- Session_2_Assignment_2.java
+++ Session_2_Assignment_2.java
@@ -34,7 +34,7 @@
 }
  
 /*
-Output of Prime Numbers example would be
+Expected Output :
 Prime numbers between 1 and 100
 1 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
 */
